3867ea803f505017528b7b79ad5bacbe3d069c2e
Dan Burzo <danburzo@gmail.com>
2013-10-25 18:35:53 +0300

    Updated list of selected streets

--- tools/generate-yaml.js
+++ tools/generate-yaml.js
@@ -4,5 +4,6 @@ streets.features.forEach(function(street) {
 	var id = street.properties.id.replace('way/', '');
 	var content = 'id: ' + id + '\n';
 	content += 'name: ' + street.properties.name + '\n';
-	fs.writeFileSync('metadata/' + id + '.yaml', content);
+	var output = 'metadata/' + id + '.yaml';
+	if (!fs.exists(output)) fs.writeFileSync(output, content);
 });
