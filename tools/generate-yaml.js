626ebebecca61978016107efac9e5c8e1f284f61
Dan Burzo <danburzo@gmail.com>
2013-10-25 19:53:23 +0300

    Removed generate-yaml tool

--- tools/generate-yaml.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var streets = JSON.parse(fs.readFileSync('json/selected-streets.json'));
-streets.features.forEach(function(street) {
-	var id = street.properties.id.replace('way/', '');
-	var content = 'id: ' + id + '\n';
-	content += 'name: ' + street.properties.name + '\n';
-	var output = 'metadata/' + id + '.yaml';
-	if (!fs.existsSync(output)) fs.writeFileSync(output, content);
-});
