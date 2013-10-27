be63f9dcf601a543ad2a4f879af8fcf426879e55
Dan Burzo <danburzo@gmail.com>
2013-10-27 14:56:36 +0200

    Created json2html task

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,10 +19,25 @@ module.exports = function(grunt) {
 					keepalive: true
 				}
 			}
+		},
+
+		json2html: {
+			options: {
+				template: 'templates/street.ejs'
+			},
+			streets: {
+				files: [{
+					expand: true,
+					cwd: 'json/metadata/',
+					src: ['*.json'],
+					dest: 'streets/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-yaml');
+	grunt.loadTasks('tasks');
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.registerTask('metadata', 'Compile street metadata from YAML source', ['yaml:streets']);
 	grunt.registerTask('default', 'Run development server', ['connect:server']);
